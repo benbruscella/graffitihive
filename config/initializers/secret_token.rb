5725368fdcc68c72c2f14f416515ab030d548d2c
Ben <benbruscella@gmail.com>
2011-02-12 13:01:40 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graffitihive::Application.config.secret_token = 'ebcf0f6b09ffa95699947d61c8df81bc598c52645acfbaa40c42957f811054662999afe16abc6840e143ab35f9e98c2ef7e4812c0d7d4fe9b4844a8fbc4339d8'
